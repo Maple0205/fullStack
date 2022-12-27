4506e09122883c44cdbcb5897f7dc03a251c1a00
Maple <sunmiao.ni@gmail.com>
2022-12-27 20:30:48 +1100

    add class method

--- test.py
+++ test.py
@@ -8,7 +8,14 @@ class Test:
     def addAll(self):
         return self.A + self.B + self.C + self.D
 
+    def minus(self,m,n):
+        return m - n
+
+    def multiply(self):
+        return self.A * self.B * self.C * self.D
 
 test = Test(6, 2, 3, 4)
 print(test.A)
 print(test.addAll())
+print(test.minus(100,1))
+print(test.multiply())
